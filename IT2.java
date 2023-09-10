c7a1ae6d71cbaed364bed3e0dd193bd6f14e27db
NirajKaushal7 <144379477+NirajKaushal7@users.noreply.github.com>
2023-09-10 12:38:16 +0530

    Delete IT2.java

--- IT2.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-class IT2
-{
-	public static void main(String ar[])
-	{
-	List<Employee> employeeList = EmployeeList.getEmployeeList();
-	System.out.println(employeeList);
-	}
-}
